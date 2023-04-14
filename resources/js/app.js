9b6a674fe6cce7baf8dcb7db9047275ddda939e4
alehhunko <teplohouses@gmail.com>
2023-04-14 16:11:04 +0300

    2

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,7 +16,9 @@ import { createApp } from 'vue';
 const app = createApp({});
 
 import ExampleComponent from './components/ExampleComponent.vue';
+import PostComponent from './components/PostComponent.vue';
 app.component('example-component', ExampleComponent);
+app.component('post-component', PostComponent);
 
 /**
  * The following block of code may be used to automatically register your
---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div>4444444444444</div>
+        <div>Name: {{ name }}</div>
+        <button @click="changeName">Click</button>
+        <ExampleComponent></ExampleComponent>
+    </div>
+</template>
+
+<script>
+import ExampleComponent from './ExampleComponent.vue';
+export default {
+    components: {
+        ExampleComponent
+    },
+
+    data() {
+        return {
+            name: 'Jan',
+        }
+    },
+
+    methods: {
+        changeName() {
+            this.name='Nick'
+        }
+    }
+}
+</script>
+
